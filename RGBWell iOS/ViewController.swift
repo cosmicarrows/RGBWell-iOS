2778eba54b1607490e705859f16db5bc5eb2ead3
Laurence Wingo <cosmicarrowsllc@cosmics-macbook-pro.local>
2018-05-03 12:45:47 -0400

    Initial Commit

---
+++ RGBWell iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGBWell iOS
+//
+//  Created by Cosmic Arrows, LLC on 5/3/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
