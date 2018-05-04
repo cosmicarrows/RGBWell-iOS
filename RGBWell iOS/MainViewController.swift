b2ef3e505222f2c74a96f949517d3dd7ffb0cb8f
Laurence Wingo <cosmicarrowsllc@cosmics-macbook-pro.local>
2018-05-04 12:35:20 -0400

    Implemented a UILabel Multiplied by UISlider Values

--- RGBWell iOS/MainViewController.swift
+++ RGBWell iOS/MainViewController.swift
@@ -11,20 +11,37 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    var num1 = 0.0
+    var num2 = 0.0
+    var num3 = 0.0
+    
+    @IBOutlet weak var calculatedLabel: UILabel!
+    
     override var nibName: String? {
         return "MainViewController"
     }
     
     @IBAction func adjustRed(sender: UISlider) {
         print("R slider's value is \(sender.value)")
+        num1 = Double(sender.value)
+        updateCalculation()
     }
     
     @IBAction func adjustGreen(sender: UISlider) {
         print("G slider's value is \(sender.value)")
+        num2 = Double(sender.value)
+        updateCalculation()
     }
     
     @IBAction func adjustBlue(sender: UISlider) {
         print("B slider's value is \(sender.value)")
+        num3 = Double(sender.value)
+        updateCalculation()
+    }
+    
+    func updateCalculation() {
+        let updatedDigits = num1 * num2 * num3
+        calculatedLabel.text = String(updatedDigits)
     }
     
 }
